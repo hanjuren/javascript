2a1477eaf3289d006f718acaadee55ae7babf2bf
hanjuren <juren528@gmail.com>
2021-09-28 16:12:53 +0900

    Day 8 chapter12

---
+++ chapter12/function.js
@@ -0,0 +1,19 @@
+// var f = function add (x, y) {
+//   return x + y;
+// };
+
+// var result = f(1, 2);
+
+// console.log(result);
+
+console.log(add(1, 3));
+// console.log(sub(2, 1));
+
+function add (x, y) {
+  return x + y;
+}
+
+const sub = (x, y) => {
+  return x + y;
+}
+console.log(sub(2, 1));
